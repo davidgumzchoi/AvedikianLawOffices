73bc63b73e0f18752742d5c029243cd30ac7f9f1
davidgumzchoi <davidgumzchoi@gmail.com>
2015-03-10 07:06:54 +0000

    third iteration responsive

--- js/application.js
+++ js/application.js
@@ -1,15 +1,6 @@
-// We have a domain name. website under construction.
-// Domain name is done through Microsoft
-// What do you want the website to accomplish?
-// - face of business
-// - brand building for our business, part of our marketing with attorneys and clients
-// - Minimal, sleek, modern, and professional
-
-// Graphics, backdrop pictures, bay area, walnut creek, east bay
-// Generic image of cool office building façade or a wall
-// Basically the homepage, attorneys
 // Something that sends an email to send a message
 
+<<<<<<< HEAD
 // Document Window Height
 var windowHeight = $(window).height();
 var wrapperHeight = $('.wrapper').height();
@@ -20,9 +11,39 @@ if (windowHeight > wrapperHeight) {
 =======
   	$('.wrapper').css({'height':(windowHeight)+'px'});
 }
+=======
+// New Update 3/9
+/*
++Logo:  We like the logo on the nav bar, in the center of the tabs.  The additional larger logo on the home page can be removed.
++Font & Font size:  The font is slightly too big throughout the website.  For “AVEDIKIAN LAW OFFICES”  we can keep the font but not in bold.
++For the rest, as Hannah suggests:  I think it would be nice if all of the words were all caps, thinner, and smaller.  There are some thinner Univers fonts you can choose from.
++Links:  Can we add a link to Yelp also?  You can remove the Facebook link (we probably will not have a Facebook page at all)
++Overall layout:  on my browser, I have to scroll down to see the bottom section of the website with our contact info.  Is there a way to see the entire page at once without having to scroll down?
++Content:  We will provide content soon – still working on that.
++Photos:  Here are the photos we’d like to use for each page of the website.
++Mac compatible:  On a Mac, the attorney overlaps with the logo.  Also, the font looks different on the Mac.
++Headshots:  We have the headshot photos – I will forward in a separate email.
+*/
+>>>>>>> 146ba87... third iteration responsive
 
 $(function(){
 	$('#header').load('header.html'); 
 	$('#footer').load('footer.html'); 
 });
+<<<<<<< HEAD
 >>>>>>> third iteration
+=======
+
+// Document Window Height
+
+if ($('#home').length) {
+	$(window).load(function() {
+		var windowHeight = $(window).height();
+		var wrapperHeight = $('.wrapper').height();
+		var footerHeight = $('#footer').outerHeight();
+		if (windowHeight > wrapperHeight) {
+		  	$('.wrapper').css({'height':(windowHeight - footerHeight)+'px'});
+		}
+	})
+}
+>>>>>>> 146ba87... third iteration responsive
