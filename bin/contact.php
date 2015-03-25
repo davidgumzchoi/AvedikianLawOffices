2dc907285e24cf4b339e26fd55fc0884361c933e
davidgumzchoi <davidgumzchoi@gmail.com>
2015-03-25 23:55:33 +0000

    final iteration

--- bin/contact.php
+++ bin/contact.php
@@ -1,6 +1,8 @@
 <?php
 	if(empty($_POST['name']) ||
 	empty($_POST['email']) ||
+	empty($_POST['number']) ||
+	empty($_POST['case']) ||
 	empty($_POST['message']) ||
 	!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 		echo "No arguments Provided!";
@@ -8,10 +10,12 @@
 	}
 	$name = $_POST['name'];
 	$email_address = $_POST['email'];
+	$number = $_POST['number'];
+	$case = $_POST['case'];
 	$message = $_POST['message'];
 	$to = 'info@avelaw.com';
 	$email_subject = 'Contact form submitted by: $name';
-	$email_body = 'You have received a new message. \n\n'. 'Here are the details:\n \nName: $name \n '. 'Email: $email_address\n Message \n $message';
+	$email_body = 'You have received a new message. \n\n'. 'Here are the details:\n \nName: $name \n '. 'Email: $email_address\n Number: $number\n Type of Case: $case\n Message \n $message';
 	$headers = 'From: Avedikian Law Offices Contact Form\n';
 	$headers .= 'Reply-To: $email_address';	
 	mail($to,$email_subject,$email_body,$headers);
